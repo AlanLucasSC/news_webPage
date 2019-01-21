b11204dff75a1cd5ea80fcbc582ad15e68f2cf65
kevyanue <kevyanuehara@hotmail.com>
2019-01-21 20:52:54 -0300

    miscellaneuos corrections

--- resources/views/shared/components/header.blade.php
+++ resources/views/shared/components/header.blade.php
@@ -2,7 +2,7 @@
 <header class="container-fluid py-3 my-0">
     <div class="row flex-nowrap justify-content-between align-items-center">
         <div class="col-4 text-center">
-            <a class="c-link text-dark display-4" href="./PageNoticia.html">NaMidiaMS</a>
+            <a class="c-link text-dark display-4" href="{{ route('index') }}">NaMidiaMS</a>
         </div>
         <div class="col-4 d-flex justify-content-end align-items-center">
             {{ $slot }}
