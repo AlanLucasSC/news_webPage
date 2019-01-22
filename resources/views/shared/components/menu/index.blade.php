0d2e938c25a4931f3b42b0c672210d8166755773
kevyanue <kevyanuehara@hotmail.com>
2019-01-21 22:09:56 -0300

    svg include within img tag

--- resources/views/shared/components/menu/index.blade.php
+++ resources/views/shared/components/menu/index.blade.php
@@ -1,11 +1,13 @@
 <!-- Menu -->
 <div class="fluid align-items-center sticky-top menu">
     <nav class="nav navbar-expand-lg d-flex bg-verde px-5">
+            
         <button class="navbar-toggler mt-1 mx-5" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon">
                 <i class="fas fa-bars text-white" aria-hidden="true"></i>
             </span>
         </button>
+        
         <div class="justify-content-center collapse navbar-collapse" id="navbarSupportedContent">
             {{ $slot }}
         </div>
